e9571ad80c7c9cb890a93b6c742a12d1205af47a
J4rly <150462106+J4rly@users.noreply.github.com>
2026-02-04 22:30:31 -0500

    feat: conexión a MySQL

--- app/db.py
+++ app/db.py
@@ -1,10 +1,16 @@
-import os
-import psycopg
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = os.getenv(
-    "DATABASE_URL",
-    "postgresql://postgres:postgres123@localhost:5432/fran_db"
-)
+# URL para MySQL (Usaremos pymysql como el "traductor")
+# root:password es el ejemplo, cámbialo si tu servidor tiene otros datos
+SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:password@localhost:3306/ventas_db"
 
-def get_connection():
-    return psycopg.connect(DATABASE_URL)
+# Creamos el motor de conexión
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
+# Configuramos la fábrica de sesiones
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+# Clase base para crear tus tablas
+Base = declarative_base()
